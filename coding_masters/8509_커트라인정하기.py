b9467cf8fd162793fac411e80be78500d51b96db
cosmosJ <jwj0392@gmail.com>
2024-03-22 17:23:34 +0900

    2024-03-22 coding test practice (coding masters 8511 cont)

--- coding_masters/8509_커트라인정하기.py
+++ coding_masters/8509_커트라인정하기.py
@@ -14,10 +14,10 @@ for target in score_list:
                 pass_list[idx - 1] = 1
                 pass_list[idx + 1] = 1
     
-    p_size = pass_list.count(1)  
+    p_size = pass_list.count(1)
     if max_score < target and p_size <= p and max_p < p_size:
         max_score = target
-        max_p = pass_list.count(1)
+        max_p = p_size
 
 if max_score != 0:
     print(max_score)
