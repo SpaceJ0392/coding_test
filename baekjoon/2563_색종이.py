8a9c49ab3ef666e9543013c1259462ca6a6cb85f
cosmosJ <jwj0392@gmail.com>
2024-04-03 23:34:38 +0900

    2024-04-03 coding test practice (baekjoon 2563 end)

--- baekjoon/2563_색종이.py
+++ baekjoon/2563_색종이.py
@@ -1,11 +1,13 @@
 n = int(input())
+zone = [[0] * 100 for _ in range(100)]
 
-square = []
 for _ in range(n):
-    x, y = map(int, input().split()) 
-    square.append([x, y, x + 10, y + 10])
+    x, y = map(int, input().split())
+    for y_idx in range(y, y + 10): 
+        for x_idx in range(x, x + 10): zone[y_idx][x_idx] = 1
+        
+cnt = 0
+for y in zone:
+    cnt += y.count(1)
 
-for idx, target in enumerate(square):
-    target_x, target_y, target_max_x, target_max_y = target
-    for sq in square[idx+1:]:
-        pass
+print(cnt)
