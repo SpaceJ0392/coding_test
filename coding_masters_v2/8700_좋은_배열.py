20bcfc1c12ec85787f78cec05fde3a957b19b5e2
cosmosJ <jwj0392@gmail.com>
2024-05-21 22:58:35 +0900

    2024-05-21 coding masters problem 31 solve 33 tried commit

--- coding_masters_v2/8700_좋은_배열.py
+++ coding_masters_v2/8700_좋은_배열.py
@@ -5,12 +5,12 @@ flag = True
 for i in range(1, n + 1):
     s_idx = target_list.find(str(i))
     b_idx = target_list.find(str(i), s_idx + 1)
-
-    if b_idx - s_idx != 1 and b_idx != n - 1:
-        middle = set(target for target in list(target_list[s_idx + 1:b_idx]))
-        back = set(target for target in list(target_list[b_idx + 1:]))
-
-        if len(middle.intersection(back)) != 0:
+    
+    if b_idx - s_idx != 1 and b_idx != 2 * n - 1:
+        middle = set(target_list[s_idx + 1:b_idx])
+        back = set(target_list[b_idx + 1:])
+        
+        if len(middle & back) != 0:
             flag = False
             break
 
---
+++ coding_masters_v2/8701_동네_한_바퀴.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s][e] = 1
+
+flag = False
+def dfs(graph, s):
+    global flag
+    if s == 1:
+        flag = True
+        return
+    
+    for e in range(n + 1):
+        if graph[s][e] == 1:
+            dfs(graph, e)
+
+for e in range(n + 1):
+    if graph[1][e] == 1:
+        dfs(graph, e)
+        
+print('YES') if flag else print('NO')
